8785963984426f73719a52cbf36db46dbe08907f
Charles Barksdale <charles.barksdale@loop.colum.edu>
2020-09-26 13:32:15 -0500

    Added First Person Script

---
+++ Assets/Scripts/MouseControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour {
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotate = 0f;
+	// Use this for initialization
+	void Start () {
+
+        Cursor.lockState = CursorLockMode.Locked;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotate -= mouseY;
+        xRotate = Mathf.Clamp(xRotate, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotate, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        
+	}
+}
