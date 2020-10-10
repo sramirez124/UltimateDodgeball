0f4671c6308e2cb1af341c954d711e03c88184c9
cbarksdale2321 <55416957+cbarksdale2321@users.noreply.github.com>
2020-10-10 13:40:19 -0500

    Added Updated code from branch testCodeBuild

---
+++ Assets/Scripts/BallPhysics/HitDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitDetection : MonoBehaviour
+{
+    public GameObject testDummy;
+    public Text scoreText;
+    public int hitPlayer = 0;
+
+
+    private void OnCollisionEnter(Collision hit)
+    {
+
+        if(hit.collider.tag == "Player")
+        {
+            scoreText.text = (hitPlayer + 1).ToString();
+            Destroy(testDummy);
+
+        }
+
+    }
+
+
+}
