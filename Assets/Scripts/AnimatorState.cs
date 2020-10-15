c1e7817c1688e2a4986d8460b5690c57011d3037
AlenLi <shilian.li@loop.colum.edu>
2020-10-14 19:56:24 -0500

    Add Animations
    Finshed a simple animator state controller and put all the movement in.

---
+++ Assets/Scripts/AnimatorState.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorState : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            animator.SetBool("run", true);
+        }
+
+        if (!Input.GetKey("w"))
+        {
+            animator.SetBool("run", false);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            animator.SetBool("jump", true);
+        }
+
+        if (!Input.GetKey("space"))
+        {
+            animator.SetBool("jump", false);
+        }
+
+        if (Input.GetKey("left shift"))
+        {
+            animator.SetBool("crouch", true);
+        }
+
+        if (!Input.GetKey("left shift"))
+        {
+            animator.SetBool("crouch", false);
+        }
+
+        if (Input.GetKey("c"))
+        {
+            animator.SetBool("dodge", true);
+        }
+
+        if (!Input.GetKey("c"))
+        {
+            animator.SetBool("dodge", false);
+        }
+
+
+        if (Input.GetKey("e"))
+        {
+            animator.SetTrigger("pickup");
+        }
+
+        if (Input.GetKey("q"))
+        {
+            animator.SetTrigger("throw");
+        }
+
+    }
+}
