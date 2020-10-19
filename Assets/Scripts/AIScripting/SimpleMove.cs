a87dd4764e8bb3eaa2c0e857d2d806caf993910c
cbarksdale2321 <55416957+cbarksdale2321@users.noreply.github.com>
2020-10-19 18:09:48 -0500

    Test AI

---
+++ Assets/Scripts/AIScripting/SimpleMove.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleMove : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask theGround, thePlayer;
+
+   
+
+    //walking
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //throwing
+    public float timeBetweenThrow;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, throwRange;
+    public bool playerInSightRange, playerInThrowRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("AI").transform;
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    private void Update()
+    {
+
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, thePlayer);
+
+        playerInThrowRange = Physics.CheckSphere(transform.position, throwRange, thePlayer);
+
+        if (!playerInSightRange && !playerInThrowRange) Patrolling();
+        if (playerInSightRange && !playerInThrowRange) AimAtPlayer();
+        if (playerInSightRange && playerInThrowRange) Throw();
+
+    }
+
+    private void Throw()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if(!alreadyAttacked)
+        {
+            //throw code here
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenThrow);
+
+        }
+
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    private void AimAtPlayer()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+        float randomX = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, theGround))
+        {
+            walkPointSet = true;
+
+        }
+            
+    }
+    private void OnCollisionEnter(Collision hit)
+    {
+        if(hit.collider.tag == "Ball")
+        {
+
+            Destroy(gameObject);
+        }
+
+    }
+}
